d705bf471397470cce42ffbd6ab6a7dc5cf1087b
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-05-22 21:15:20 -0300

    feat: Adicionando o método existsByNomeLista na iterface ListaFavoritosRepository

--- serratecFlix/src/main/java/org/serratec/serratecFlix/repository/FilmeRepository.java
+++ serratecFlix/src/main/java/org/serratec/serratecFlix/repository/FilmeRepository.java
@@ -1,10 +1,18 @@
 package org.serratec.serratecFlix.repository;
 
+import java.util.List;
+
 import org.serratec.serratecFlix.entity.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FilmeRepository extends JpaRepository <Filme, Long> {
       Boolean existsByTitulo();
+
+      @Query("SELECT f FROM Filme f ORDER BY f.notaMedia DESC")
+      List<Filme> findRankingFilmes();
+
+     
 }
