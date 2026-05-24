192a9ebf4fe9b36dc26585da0e3c55316d262dc8
marcospmelloo <marcospoliveiramello@gmail.com>
2026-05-24 20:52:18 -0300

    feat: implementa controllers AvaliacaoSerie e ListaFavoritos

--- serratecFlix/src/main/java/org/serratec/serratecFlix/controller/AvaliacaoSerieController.java
+++ serratecFlix/src/main/java/org/serratec/serratecFlix/controller/AvaliacaoSerieController.java
@@ -1,4 +1,41 @@
 package org.serratec.serratecFlix.controller;
 
+import org.serratec.serratecFlix.DTO.RequestDTO.AvaliacaoSerieRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("avaliacoes-series")
 public class AvaliacaoSerieController {
+    
+    @Autowired
+    private AvaliacaoSerieService avaliacaoSerieService;
+
+    @GetMapping
+    public ResponseEntity<List<AvaliacaoSerieResponseDTO>> listarAvaliacoes() {
+        return ResponseEntity.ok(avaliacaoSerieService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AvaliacaoSerieResponseDTO> listarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(avaliacaoSerieService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<AvaliacaoSerieResponseDTO> criarAvaliacao(@RequestBody AvaliacaoSerieRequestDTO request) {
+        return ResponseEntity.status(201).body(avaliacaoSerieService.cadastrar(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AvaliacaoSerieResponseDTO> atualizarAvaliacao(@PathVariable Long id,
+                                                               @RequestBody AvaliacaoSerieRequestDTO request) {
+        return ResponseEntity.ok(avaliacaoSerieService.atualizar(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<AvaliacaoSerieResponseDTO> deletarAvaliacao(@PathVariable Long id) {
+        avaliacaoSerieService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- serratecFlix/src/main/java/org/serratec/serratecFlix/controller/ListaFavoritosController.java
+++ serratecFlix/src/main/java/org/serratec/serratecFlix/controller/ListaFavoritosController.java
@@ -1,4 +1,41 @@
 package org.serratec.serratecFlix.controller;
 
+import org.serratec.serratecFlix.DTO.RequestDTO.ListaFavoritosRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/listas-favoritos")
 public class ListaFavoritosController {
+    
+    @Autowired
+    private ListaFavoritosService listaFavoritosService;
+
+    @GetMapping
+    public ResponseEntity<List<ListaFavoritosResponseDTO>> listarLista() {
+        return ResponseEntity.ok(listaFavoritosService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ListaFavoritosResponseDTO> listarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(listaFavoritosService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ListaFavoritosResponseDTO> criarLista(@RequestBody ListaFavoritosRequestDTO request) {
+        return ResponseEntity.status(201).body(listaFavoritosService.cadastrar(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ListaFavoritosResponseDTO> atualizarLista(@PathVariable Long id,
+                                                               @RequestBody ListaFavoritosRequestDTO request) {
+        return ResponseEntity.ok(listaFavoritosService.atualizar(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ListaFavoritosResponseDTO> deletarLista(@PathVariable Long id) {
+        listaFavoritosService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
