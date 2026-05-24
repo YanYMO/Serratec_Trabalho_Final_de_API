eeddcac2bb95326ff96191591a3914da18fbac48
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-05-24 15:24:24 -0300

    fix: Apagando imports que não estavam sendo usados

--- serratecFlix/src/main/java/org/serratec/serratecFlix/entity/AvaliacaoSerie.java
+++ serratecFlix/src/main/java/org/serratec/serratecFlix/entity/AvaliacaoSerie.java
@@ -2,9 +2,6 @@ package org.serratec.serratecFlix.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- serratecFlix/src/main/java/org/serratec/serratecFlix/entity/Serie.java
+++ serratecFlix/src/main/java/org/serratec/serratecFlix/entity/Serie.java
@@ -4,10 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
