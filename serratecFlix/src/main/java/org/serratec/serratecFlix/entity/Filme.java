64b833c14c8f793c7da9f2d35501d6eefa3da72d
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-05-24 14:48:46 -0300

    feat: Adicionando os relacionamentos de prêmios nas entidades Filme e série

--- serratecFlix/src/main/java/org/serratec/serratecFlix/entity/Filme.java
+++ serratecFlix/src/main/java/org/serratec/serratecFlix/entity/Filme.java
@@ -1,6 +1,9 @@
 package org.serratec.serratecFlix.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -13,6 +16,8 @@ import lombok.Setter;
 import org.serratec.serratecFlix.enums.ClassificacaoIndicativa;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -26,30 +31,36 @@ public class Filme {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Este campo precisa ser preenchido")
-    @Size(max = 50)
     @Column(name = "titulo", nullable = false, length = 60)
     private String titulo;
 
-    @NotBlank(message = "Este campo precisa ser preenchido")
     @Column(name = "descricao", nullable = false)
     private String descricao;
 
-    @NotNull(message = "Este campo precisa ser preenchido")
     @Column(name = "duracao_em_minutos", nullable = false)
     private Integer duracaoMinutos;
 
-    @NotNull(message = "Este campo precisa ser preenchido")
-    @Past
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     @Column(name = "data_de_lancamento", nullable = false)
-    private LocalDate dataLançamento;
+    private LocalDate dataLancamento;
 
-    @NotNull(message = "Este campo precisa ser preenchido")
-    @Enumerated(EnumType.STRING)
     @Column(name = "classificacao_indicativa", nullable = false)
     private ClassificacaoIndicativa classificacao;
 
     @Column(name = "nota_media", nullable = true)
     private Double notaMedia;
+
+    @OneToMany(mappedBy = "filme", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<AvaliacaoFilme> avaliacoesFilmes;
+
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinTable(name = "filme_categoria",
+            joinColumns = @JoinColumn(name = "id_filme"),
+            inverseJoinColumns = @JoinColumn(name = "id_categoria"))
+    @JsonManagedReference
+    private List<Categoria> categorias;
+
+    @OneToMany(mappedBy = "filme", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Premio> premios = new ArrayList<>();
 }
--- serratecFlix/src/main/java/org/serratec/serratecFlix/entity/Serie.java
+++ serratecFlix/src/main/java/org/serratec/serratecFlix/entity/Serie.java
@@ -1,6 +1,8 @@
 package org.serratec.serratecFlix.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -12,6 +14,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -25,29 +29,31 @@ public class Serie {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Este campo precisa ser preenchido")
-    @Size(max = 60)
+   
     @Column(name = "titulo", nullable = false, length = 60)
     private String titulo;
 
-    @NotBlank(message = "Este campo precisa ser preenchido")
+    
     @Column(name = "descricao", nullable = false)
     private String descricao;
 
-    @NotNull(message = "Este campo precisa ser preenchido")
+    
     @Column(name = "temporadas", nullable = false)
     private Integer temporadas;
 
-    @NotNull(message = "Este campo precisa ser preenchido")
+    
     @Column(name = "epsodios", nullable = false)
     private Integer episodios;
 
-    @NotNull(message = "Este campo precisa ser preenchido")
-    @Past
+   
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     @Column(name = "data_de_lancamento", nullable = false)
     private LocalDate dataLançamento;
 
     @Column(name = "nota_media", nullable = true)
     private Double notaMedia;
+
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Premio> premios = new ArrayList<>();
 }
