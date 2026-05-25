3df1d9d6c2cca5b17eb80831073868936a6a6718
YanYMO <yanmartinsd3@gmail.com>
2026-05-24 23:23:58 -0300

    feat: Adicionando a Classe LoginDTO que irá receber os dados do usuário que irá acessar o sistema.

---
+++ serratecFlix/src/main/java/org/serratec/serratecFlix/DTO/RequestDTO/LoginDTO.java
@@ -0,0 +1,25 @@
+package org.serratec.serratecFlix.dto.RequestDTO;
+
+public class LoginDTO {
+
+    private String username;
+
+    private String passworld;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassworld() {
+        return passworld;
+    }
+
+    public void setPassworld(String passworld) {
+        this.passworld = passworld;
+    }
+}
