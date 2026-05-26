b849954942e76643768d387a8346d62481e9ce2a
CarEduCar <carloscarvalho201532@gmail.com>
2026-05-26 20:08:03 -0300

    chore: envio temporário para teste de outra branch

---
+++ serratecFlix/src/main/java/org/serratec/serratecFlix/controller/ExperienciaController.java
@@ -0,0 +1,15 @@
+package org.serratec.serratecFlix.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Experiência", description = "Acesso às Experiências")
+@RestController
+@RequestMapping("/experiencia")
+public class ExperienciaController {
+	
+	
+
+}
