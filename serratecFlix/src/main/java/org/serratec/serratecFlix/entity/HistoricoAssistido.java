4fb5c72d231380f5dc63e0e6e2f7409222251b85
marcospmelloo <marcospoliveiramello@gmail.com>
2026-05-26 18:46:15 -0300

    feat: cria entity HistoricoAssistido com relacionamentos

---
+++ serratecFlix/src/main/java/org/serratec/serratecFlix/entity/HistoricoAssistido.java
@@ -0,0 +1,42 @@
+package org.serratec.serratecFlix.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.serratec.serratecFlix.enums.StatusAssistido;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "historico_assistido")
+public class HistoricoAssistido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "idUsuario", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "idFilme")
+    private Filme filme;
+
+    @ManyToOne
+    @JoinColumn(name = "idUsuario")
+    private Serie serie;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusAssistido statusAssistido;
+
+    @Column(nullable = false)
+    private LocalDate data;
+}
