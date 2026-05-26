366c843e653de70eabd7bc975ea81a9584a6a26a
CarEduCar <carloscarvalho201532@gmail.com>
2026-05-25 21:01:33 -0300

    feat: criacao do respose da Experiencia

---
+++ serratecFlix/src/main/java/org/serratec/serratecFlix/dto/responsedto/ExperienciaResponseDTO.java
@@ -0,0 +1,16 @@
+package org.serratec.serratecFlix.dto.responsedto;
+
+public class ExperienciaResponseDTO {
+	
+	Integer xp;
+	
+	Integer nivel;
+
+	public ExperienciaResponseDTO(Integer exp, Integer level) {
+		super();
+		this.xp = exp;
+		this.nivel = level;
+	}
+	
+	
+}
